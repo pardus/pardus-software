d2db84ef4e3c5562ac936692111f09421446d7e8
Fatih Altun <fatih.altun@pardus.org.tr>
2022-06-23 00:04:23 +0300

    Add update-translations script

---
+++ update-translations.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+langs=("tr" "pt")
+
+if ! command -v xgettext &> /dev/null
+then
+	echo "xgettext could not be found."
+	echo "you can install the package with 'apt install gettext' command on debian."
+	exit
+fi
+
+
+echo "updating pot file"
+xgettext -o po/pardus-software.pot --files-from=po/files
+
+for lang in ${langs[@]}; do
+	if [[ -f po/$lang.po ]]; then
+		echo "updating $lang.po"
+		msgmerge -o po/$lang.po po/$lang.po po/pardus-software.pot
+	else
+		echo "creating $lang.po"
+		cp po/pardus-software.pot po/$lang.po
+	fi
+done
