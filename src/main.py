e124900421564980a28bc0e76fef699b1390a70e
Fatih Altun <fatih.altun@pardus.org.tr>
2023-01-09 12:01:13 +0300

    Add license to main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 18 14:53:00 2020
+Copyright: 2021 Pardus Developers <dev@pardus.org.tr>
+ This file is part of pardus-software.
+ pardus-software is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ option) any later version.
 
+ Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with Foobar. If not, see
+ <https://www.gnu.org/licenses/>.
+
+Created on Fri Sep 18 14:53:00 2020
 @author: fatih
 """
 
