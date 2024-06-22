5bc4823ff1e011daa20db1d8be3b4ba8f801e32a
bariscodefxy <bariscodefx@protonmail.com>
2024-06-22 22:37:54 +0300

    ProcessManager: add comment header & remove useless things

--- src/ProcessManager.py
+++ src/ProcessManager.py
@@ -1,10 +1,14 @@
+"""
+Created on Sat Feb 3 17:31:00 2024
+
+@author: bariscodefxy
+"""
 import psutil
 import os
 
 class ProcessManager():
 
 	def __init__(self):
-		self.lastpidfile = "/tmp/pardus-software-action.pid"
 		self.install_cmd = "apt install -yq -o APT::Status-Fd=2"
 
 	def get_running_process(self):
@@ -25,20 +29,4 @@ class ProcessManager():
 		if self.install_cmd in string:
 			return string.split(self.install_cmd)[1]
 
-		return None
-
-	def write_last_pid(self, value = ""):
-		with open(self.lastpidfile, 'w') as file:
-			file.write(str(value))
-
-	def get_last_pid(self):
-		if os.path.isfile(self.lastpidfile):
-			with open(self.lastpidfile, 'r') as file:
-				data = file.readline()
-				if not data:
-					return None
-				if not self.is_running_process(data):
-					return None
-
-				return data
 		return None
