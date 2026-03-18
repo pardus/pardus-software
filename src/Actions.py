c45a8695892f1483c72b47e1ce33932349be8b39
Fatih Altun <fatih.altun@pardus.org.tr>
2026-03-18 18:07:33 +0300

    Fix kill func

--- src/Actions.py
+++ src/Actions.py
@@ -9,7 +9,7 @@ Created on Fri Sep 18 14:53:00 2020
 import os
 import subprocess
 import sys
-
+import pwd
 import apt
 import apt_pkg
 import psutil
@@ -77,25 +77,61 @@ def main():
 
     def kill(pid):
         print("kill function working for: {} pid".format(pid))
-        kill_dic = {}
-        parent = psutil.Process(int(pid))
-        for child in parent.children(recursive=True):
-            kill_dic[child.name()] = child.pid
 
-        keys = kill_dic.keys()
-        if "dpkg" in keys or "dpkg-deb" in keys:
+        if int(pid) < 100:
+            print(f"kill func: cannot kill, pid: {pid}")
+            return
+
+        parent = psutil.Process(int(pid))
+
+        parent_cmdline = "".join(parent.cmdline())
+        if "pardus-software/src/Actions.py" not in parent_cmdline:
+            print("kill func: not pardus-software process")
+            return
+
+        install_phase = any("dpkg" in child.exe() for child in parent.children(recursive=True))
+        print(f"Install phase: {install_phase}")
+
+        if install_phase:
             print("You can't cancel the operation because download completed and install in progress.")
-        elif "http" in keys or "https" in keys:
-            print("download operation is cancelling")
-            for child_name, child_pid in kill_dic.items():
-                print("child killing!!! name: {} - pid: {}".format(child_name, child_pid))
+            return
+
+        kill_dic = {}
+        for child in parent.children(recursive=True):
+            try:
+                child_name = child.name()
+                child_pid = child.pid
+                child_exe = child.exe()
+                child_uid = child.uids().real
+                child_user = pwd.getpwuid(child_uid).pw_name
+                child_cmdline = " ".join(child.cmdline())
+                print(f"Checking child: {child_name} PID={child_pid} exe={child_exe} user={child_user} cmdline={child_cmdline}")
+
+                # apt child
+                if child_name == "apt" and child_exe == "/usr/bin/apt" and "apt" in child_cmdline:
+                    print(f"Adding apt child to kill list: {child_name} PID={child_pid}")
+                    kill_dic[child_pid] = child_name
+
+                # apt download child
+                elif child_user == "_apt" and \
+                     (child_exe.startswith("/usr/lib/apt/methods")) and \
+                     (child_cmdline.startswith("/usr/lib/apt/methods")):
+                    print(f"Adding apt download child to kill list: {child_name} PID={child_pid}")
+                    kill_dic[child_pid] = child_name
+
+            except Exception as e:
+                print(f"Error reading child PID={child.pid}: {e}")
+                continue
+
+        if kill_dic:
+            print("Download operation is cancelling")
+            for child_pid, child_name in kill_dic.items():
+                print(f"child killing: name: {child_name} - pid: {child_pid}")
                 psutil.Process(child_pid).kill()
-            print("parent killing!!! name: {} - pid: {}".format(parent.name(), parent.pid))
+            print(f"parent killing name: {parent.name()} - pid: {parent.pid}")
             parent.kill()
         else:
-            print("There is something wrong.")
-            for child_name, child_pid in kill_dic.items():
-                print("child, name: {} - pid: {}".format(child_name, child_pid))
+            print("No apt/_apt download child found. Nothing to kill.")
 
     if len(sys.argv) > 1:
         if sys.argv[1] == "kill":
